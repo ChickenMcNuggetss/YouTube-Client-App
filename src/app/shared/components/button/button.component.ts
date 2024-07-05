bd643a7021668cec59ac165428a95f7411a58557
KarinaI <7karina.ibr@gmail.com>
2024-07-05 12:50:17 +0300

    feat: add button component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-button',
-  standalone: true,
-  imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
 })
 export class ButtonComponent {
-
+  @Input() titleButton: string = ''
 }
