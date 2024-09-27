95480b6208745ee78504117063f7c900e8dc5c77
KarinaI <7karina.ibr@gmail.com>
2024-09-27 13:33:08 +0300

    refactor: refactor jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,14 +4,14 @@ const { defaultTransformerOptions } = require('jest-preset-angular/presets');
 
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  setupFilesAfterEnv: ['src/setup-jest.ts'],
   transformIgnorePatterns: ['node_modules/(?!@angular|@ngrx)'],
   transform: {
     '^.+\\.(ts|js|mjs|html|svg)$': [
         'jest-preset-angular',
         {
             ...defaultTransformerOptions,
-            tsconfig: '<rootDir>/tsconfig.spec.json',
+            tsconfig: './tsconfig.spec.json',
             stringifyContentPathRegex: '\\.(html|svg)$',
             isolatedModules: true,
         },
